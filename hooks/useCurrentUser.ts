67604a6828952a2ec3e1272fc9ec37da7f9fd07a
Adam Drago <atdrago@gmail.com>
2022-11-17 19:08:12 -0500

    fix: Rerun user query when token changes

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,21 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 
+import { useCookies } from "hooks/useCookiesContext";
 import { fetchWithAuth } from "lib/client/fetchWithAuth";
 import { TwitchUser } from "types";
 
 export const useCurrentUser = () => {
-  return useQuery(
-    ["user"],
-    async () => {
-      const usersResponseJson = await fetchWithAuth<{ data: TwitchUser[] }>(
-        "https://api.twitch.tv/helix/users",
-        {
-          method: "GET",
-        },
-        "user"
-      );
+  const { cookies } = useCookies();
 
-      return usersResponseJson.data[0];
+  return useQuery(
+    // Make the user-access-token a part of the user query key so it refreshes
+    // when the token changes
+    ["user", cookies["user-access-token"]],
+    async () => {
+      try {
+        const usersResponseJson = await fetchWithAuth<{ data: TwitchUser[] }>(
+          "https://api.twitch.tv/helix/users",
+          {
+            method: "GET",
+          },
+          "user"
+        );
+
+        return usersResponseJson.data[0];
+      } catch {
+        return null;
+      }
     },
     {
       retry: false,
