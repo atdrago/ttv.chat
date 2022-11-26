f65f1ba4f762e6b376532f04f934c47d8f49d9ef
Adam Drago <atdrago@gmail.com>
2022-11-25 22:44:15 -0500

    fix: Color scheme not changing with system

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -6,11 +6,19 @@ export const useColorScheme = (): ColorScheme => {
   const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
 
   useEffect(() => {
-    setColorScheme(
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? "dark"
-        : "light"
-    );
+    const darkMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    setColorScheme(darkMediaQuery.matches ? "dark" : "light");
+
+    const handleChange = (event: MediaQueryListEvent) => {
+      setColorScheme(event.matches ? "dark" : "light");
+    };
+
+    darkMediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      darkMediaQuery.removeEventListener("change", handleChange);
+    };
   }, []);
 
   return colorScheme;
